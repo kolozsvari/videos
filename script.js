3da0243334e8402dc79cb9f6abb3b4c9ac7e184d
kolozsvari <kolozsvarikrisztinajanka@gmail.com>
2023-05-03 13:24:27 +0200

    videók

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-<script>
+function videólejatszo(x)
 
-  function videólejatszo(x)
+
+
   {
-    if(x==1)  videó.src="https://www.youtube.com/embed/eMsKXMfs6gw?autoplay=1&rel=0"
-    if(x==2)  videó.src="https://www.youtube.com/embed/Z_JIbN22ckE?autoplay=1&rel=0"
-    if(x==3)  videó.src="https://www.youtube.com/embed/OkUE6YxaNPo?autoplay=1&rel=0"
-    if(x==4)  videó.src="https://www.youtube.com/embed/OkUE6YxaNPo?autoplay=1&rel=0"
+    if(x==1)  videó.src="https://www.youtube.com/embed/W96-n3M9ntg?autoplay=1&rel=0"
+    if(x==2)  videó.src="https://www.youtube.com/embed/_zL9Q10YMVQ?autoplay=1&rel=0"
+    if(x==3)  videó.src="https://www.youtube.com/embed/KgZMezU7O2I?autoplay=1&rel=0"
+    if(x==4)  videó.src="https://www.youtube.com/embed/hNLHQWppa1E?autoplay=1&rel=0"
   }
-
-</script>
